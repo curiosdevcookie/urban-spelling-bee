a4cfc19f05462ffd9b3d369822b75d5768cdd708
Ariadne Engelbrecht <ariadne.engelbrecht@thenativeweb.io>
2022-08-07 16:24:42 +0200

    Destructure API response

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-
-
-
 const APIurl = 'https://api.urbandictionary.com/v0/define?term=';
 
 
@@ -9,28 +6,64 @@ async function fetchData(term) {
   if (!response.ok) {
     console.log(response.status, response.statusText);
   } else {
-    const data = await response.json();
-    console.log(data);
+    const {
+      list: [{
+        definition
+      }]
+    } = await response.json();
+    console.log(definition);
   }
 }
 
 
+const letter1 = document.getElementById('b1');
+letter1.addEventListener('click', letterValueInput);
+
+const letter2 = document.getElementById('b2');
+letter2.addEventListener('click', letterValueInput);
+
+const letter3 = document.getElementById('b3');
+letter3.addEventListener('click', letterValueInput);
+
+const letter4 = document.getElementById('b4');
+letter4.addEventListener('click', letterValueInput);
+
+const letter5 = document.getElementById('b5');
+letter5.addEventListener('click', letterValueInput);
+
+const letter6 = document.getElementById('b6');
+letter6.addEventListener('click', letterValueInput);
+
+const letter7 = document.getElementById('b7');
+letter7.addEventListener('click', letterValueInput);
+
+function letterValueInput() {
+  const input = document.getElementById('inputPlaceholder');
+  input.value += this.innerText;
+}
+
+const deleteButton = document.getElementById('delete');
+deleteButton.addEventListener('click', deleteLetter);
+
+function deleteLetter() {
+  const input = document.getElementById('inputPlaceholder');
+  input.value = input.value.slice(0, -1);
+};
+
+const buttonSubmit = document.getElementById('submit');
+buttonSubmit.addEventListener('click', submitWord);
 
 function submitWord() {
   const term = document.getElementById('inputPlaceholder').value;
   fetchData(term);
 }
 
-const buttonSubmit = document.getElementById('submit');
-buttonSubmit.addEventListener('click', submitWord);
+const buttonDefinition = document.getElementById('wordDefinition');
+buttonDefinition.addEventListener('click', definitionClick);
 
-
-const letter1 = document.getElementById('b1');
-letter1.addEventListener('click', letterValueLetter2);
-
-function letterValueLetter2() {
-  const letter2 = document.getElementById('b2');
-  letter2.value = document.getElementById('inputPlaceholder');
-}
-const letter2 = document.getElementById('b2').innerText;
-console.log(letter2.innerText);
+function definitionClick() {
+  const definitionWord = document.getElementById('inputPlaceholder').value;
+  fetchData(definitionWord);
+  const definitionArea = document.getElementById('definitionArea');
+  definitionArea.innerText = `${definition}`;
+};
